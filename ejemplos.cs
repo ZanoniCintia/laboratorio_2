ec92cc688df1e17622d585d15deb93d87ffbb49d
alumno <alumno@utnfra.com.ar>
2019-08-21 20:53:43 -0300

    ejemplos_clase_01

---
+++ ejemplos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Primera clase";
+            Console.BackgroundColor = ConsoleColor.Blue;
+            string nombre = "Cintia";
+            string apellido = "Zanoni";
+            Console.WriteLine("Hola {0} {1}",nombre,apellido);
+            Console.ReadKey();
+        }
+    }
+}
