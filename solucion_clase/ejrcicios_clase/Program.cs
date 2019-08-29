4ff172690bcb1fc46eb2788e9ab4a8cbd323e36d
alumno <alumno@utnfra.com.ar>
2019-08-28 21:17:27 -0300

    ejercicio 11,sin terminar

---
+++ solucion_clase/ejrcicios_clase/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejrcicios_clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 11";
+            int numero = default(int);
+            int i = 0;
+            int acumulador = 0;
+            int Maximo = default(int);
+            int Minimo = default(int);
+            string entrada = string.Empty;
+
+           for(i=0;i<=10;i++)
+            {
+                Console.WriteLine("Ingrese diez numeros  entre -100 y 100");
+                if(!!Int32.TryParse(entrada, out numero) || !validacion.validar(numero,-100,100))
+                {
+                    Console.WriteLine("ERROR,solo ingrese numeros entre -100 y 100");
+                }
+                else
+                {
+                    acumulador += numero;
+                }
+
+                if()
+            }
+
+
+           
+
+        
+        }
+
+    }
+}
---
+++ solucion_clase/ejrcicios_clase/validacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejrcicios_clase
+{
+    public class validacion
+    {
+        public static bool validar(int valor, int min, int max)
+        {
+           
+            if(valor < min || valor > max)
+            {
+                return false;
+            }
+           
+            return true;
+        }
+    }
+}
