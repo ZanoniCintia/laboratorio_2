7544057bad4aa699721f4839b724c3623aa0e680
zanonicintia <cintu94@hotmail.com>
2019-08-29 15:50:37 -0300

    ejercicio_12

---
+++ solucion_clase/ejercicio_12/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_12
+{
+    public class ValidarRespuesta
+    {
+        public static bool validaS_N(char c)
+        {
+            bool retorno = false;
+            if (c == 's' || c=='S')
+            {
+                retorno=true;
+            }
+            return retorno;
+        }
+    }
+}
---
+++ solucion_clase/ejercicio_12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 12";
+            int numero = default(int);
+            int acumulador = 0;
+            char respuesta = 'n';
+            do
+            {
+                Console.WriteLine("Ingrese un numero entero");
+                while (!int.TryParse(Console.ReadLine(), out numero))
+                {
+                    Console.WriteLine("ERROR,solo ingrese numeros enteros");
+                }
+                acumulador += numero;
+                Console.WriteLine("Desea continuar? s/n");
+            } while (ValidarRespuesta.validaS_N(respuesta));
+
+            Console.WriteLine("La suma de todos los numeros ingresados es :{0} ", acumulador);
+        }
+    }
+}
