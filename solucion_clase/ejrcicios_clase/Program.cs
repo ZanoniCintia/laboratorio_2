73138e7af78d0ca959797e269f5fbf3f3a7e50cc
alumno <alumno@utnfra.com.ar>
2019-08-28 21:46:04 -0300

    ejercicio 11 sin terminar

--- solucion_clase/ejrcicios_clase/Program.cs
+++ solucion_clase/ejrcicios_clase/Program.cs
@@ -21,7 +21,7 @@ namespace ejrcicios_clase
            for(i=0;i<=10;i++)
             {
                 Console.WriteLine("Ingrese diez numeros  entre -100 y 100");
-                if(!!Int32.TryParse(entrada, out numero) || !validacion.validar(numero,-100,100))
+                if (!!Int32.TryParse(entrada, out numero) || !validacion.validar(numero, -100, 100))
                 {
                     Console.WriteLine("ERROR,solo ingrese numeros entre -100 y 100");
                 }
@@ -29,14 +29,31 @@ namespace ejrcicios_clase
                 {
                     acumulador += numero;
                 }
-
-                if()
+                
+            }
+            if (i == 0)
+            {
+                Maximo = numero;
+                Minimo = numero;
+            }
+            else if (numero > Maximo)
+            {
+                Maximo = numero;
+            }
+            else if (numero < Minimo)
+            {
+                Minimo = numero;
             }
 
+            Console.WriteLine("El minimo es [0] :", Minimo);
+            Console.WriteLine("El maximo es [0] :", Maximo);
+            Console.WriteLine("El promedio es [0] :", acumulador/i);
+            Console.ReadKey();
 
-           
 
-        
+
+
+
         }
 
     }
