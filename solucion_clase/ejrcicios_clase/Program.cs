5f0b37c4eeb6f7eefc15be4d71ba4bbd4702fb75
alumno <alumno@utnfra.com.ar>
2019-08-28 22:02:54 -0300

    ejercicio 11_terminado

--- solucion_clase/ejrcicios_clase/Program.cs
+++ solucion_clase/ejrcicios_clase/Program.cs
@@ -16,38 +16,40 @@ namespace ejrcicios_clase
             int acumulador = 0;
             int Maximo = default(int);
             int Minimo = default(int);
-            string entrada = string.Empty;
+            int iteraciones = 10;
+            //string entrada = string.Empty;
 
-           for(i=0;i<=10;i++)
+           for(i=0;i<iteraciones;i++)
             {
-                Console.WriteLine("Ingrese diez numeros  entre -100 y 100");
-                if (!!Int32.TryParse(entrada, out numero) || !validacion.validar(numero, -100, 100))
+                //int numero;
+                Console.WriteLine("Ingrese un numero  entre -100 y 100");
+                while (!int.TryParse(Console.ReadLine(), out numero) || !validacion.validar(numero, -100, 100))
                 {
                     Console.WriteLine("ERROR,solo ingrese numeros entre -100 y 100");
                 }
-                else
-                {
-                    acumulador += numero;
-                }
                 
-            }
-            if (i == 0)
-            {
-                Maximo = numero;
-                Minimo = numero;
-            }
-            else if (numero > Maximo)
-            {
-                Maximo = numero;
-            }
-            else if (numero < Minimo)
-            {
-                Minimo = numero;
-            }
+                    acumulador += numero;
+                
+                if (i == 0)
+                {
+                    Maximo = numero;
+                    Minimo = numero;
+                }
+                else if (numero > Maximo)
+                {
+                    Maximo = numero;
+                }
+                else if (numero < Minimo)
+                {
+                    Minimo = numero;
+                }
 
-            Console.WriteLine("El minimo es [0] :", Minimo);
-            Console.WriteLine("El maximo es [0] :", Maximo);
-            Console.WriteLine("El promedio es [0] :", acumulador/i);
+            }
+            
+
+            Console.WriteLine("El minimo es :{0} ", Minimo);
+            Console.WriteLine("El maximo es :{0} ", Maximo);
+            Console.WriteLine("El promedio es :{0} ", acumulador/iteraciones);
             Console.ReadKey();
 
 
