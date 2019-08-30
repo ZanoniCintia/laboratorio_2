35f5893f5c20ba2c9a4551d7804bef4c64841294
alumno <alumno@utnfra.com.ar>
2019-08-29 21:30:25 -0300

    ejercicio_16

---
+++ solucion_clase/ConsoleApp1/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_16
+{
+    public class Alumno
+    {
+        #region Atributo
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+        private string apellido;
+        private int legajo;
+        private string nombre;
+        #endregion
+
+        public Alumno(string nombre,string apellido,int legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+
+        }
+        public void CalcularFinal()
+        {
+            Random final = new Random();
+            if (this.nota1 >= 4 && this.nota2 >=4)
+            {
+                
+                this.notaFinal= final.Next(1, 11);
+
+            }
+            else
+            {
+                this.notaFinal = -1;
+            }
+            
+        }
+        /// <summary>
+        /// carga las notas del alumno
+        /// </summary>
+        /// <param name="notaUno">nota del primer parcial</param>
+        /// <param name="notaDos">nota del segundo parcial</param>
+        public void Estudiar(byte notaUno,byte notaDos)
+        {
+            this.nota1 = notaUno;
+            this.nota2 = notaDos;
+        }
+        public string Mostrar()
+        {
+            string mostrar = string.Empty;
+            mostrar += string.Format("Nombre: {0}\n", this.nombre);
+            mostrar += string.Format("Apellido: {0}\n", this.apellido);
+            mostrar += string.Format("Legajo:{0}\n", this.legajo);
+            mostrar += string.Format("Nota final:{0}\n", this.notaFinal);
+            return mostrar;
+        }
+
+
+    }
+}
---
+++ solucion_clase/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_16
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 16";
+            string entrada = string.Empty;
+            
+            //instancio e inicializo campos del alumno
+            Alumno alumno1 = new Alumno("Carolina", "Stomboli", 22);
+            Alumno alumno2 = new Alumno("Daniela", "Moreno", 26);
+            Alumno alumno3 = new Alumno("Matias", "Moll", 27);
+
+            //cargo las notas con el metodo estudiar
+            alumno1.Estudiar(3, 8);
+            alumno2.Estudiar(4, 6);
+            alumno3.Estudiar(4, 6);
+
+            //genero la nota final
+            alumno1.CalcularFinal();
+            alumno2.CalcularFinal();
+            alumno3.CalcularFinal();
+
+            //mostrar los datos
+            Console.WriteLine(alumno1.Mostrar());
+            Console.WriteLine(alumno2.Mostrar());
+            Console.WriteLine(alumno3.Mostrar());
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
