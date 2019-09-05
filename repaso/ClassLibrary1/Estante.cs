7825d6cb4cf9148abba7d4784c5bf8a4d93fbe91
alumno <alumno@utnfra.com.ar>
2019-09-04 21:45:46 -0300

    ejercicio_05_campus

---
+++ repaso/ClassLibrary1/Estante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Estante
+    {
+        private int ubicacionEstante;
+        private Producto[] productos;
+
+        public Producto GetProductos()
+        {
+            return proproductos;
+        }
+
+
+            
+    }
+}
---
+++ repaso/ClassLibrary1/Producto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Producto
+    {
+        #region Atributo
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+        #endregion 
+
+        private Producto(string marca,string codigoDeBarra,float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigoDeBarra;
+            this.precio = precio;
+
+        }
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            string mostrar = string.Empty;
+
+            mostrar += string.Format("Marca: {0}\n", p.marca);
+            mostrar += string.Format("Codigo de Barras : {0}\n", p.codigoDeBarra);
+            mostrar += string.Format("precio :{0}\n", p.precio);
+
+            return mostrar;
+
+
+        }
+
+    }
+}
