314f6ab1acfda85afcdd7a06d017975c0215a1b7
alumno <alumno@utnfra.com.ar>
2019-09-18 21:30:55 -0300

    terminar ejercicio 31

---
+++ atencionAlCliente/atencionAlCliente/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atencionAlCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
