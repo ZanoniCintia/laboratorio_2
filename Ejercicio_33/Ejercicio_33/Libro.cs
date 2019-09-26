1ed993a8e3d439c8a6336d2dc1e224c639103a1d
zanonicintia <cintu94@hotmail.com>
2019-09-26 16:07:18 -0300

    ejercicio_33_Libro

--- Ejercicio_33/Ejercicio_33/Libro.cs
+++ Ejercicio_33/Ejercicio_33/Libro.cs
@@ -8,5 +8,39 @@ namespace Ejercicio_33
 {
     class Libro
     {
+        private List<string> paginas;
+
+        public Libro()
+        {
+            this.paginas = new List<string>();
+        }
+        public string this[int i]
+        {
+            get
+            {
+                ;
+                if (i >= 0 && i < this.paginas.Count)
+                {
+                    return this.paginas[i];
+                }
+                else
+                {
+                    return string.Empty;
+                }
+
+            }
+            set
+            {
+                if (paginas.Count == i && i > this.paginas.Count)
+                {
+                    this.paginas[i] = value;
+                }
+                else
+                {
+                    this.paginas.Add(value);
+                }
+            }
+
+        }
     }
 }
