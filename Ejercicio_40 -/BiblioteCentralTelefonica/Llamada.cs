95d0d131a5b0828cf299bd15c08639177124d650
alumno <alumno@utnfra.com.ar>
2019-09-26 22:10:01 -0300

    ejercicio 40

---
+++ Ejercicio_40 -/BiblioteCentralTelefonica/Llamada.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioteCentralTelefonica
+{
+    public abstract class Llamada
+    {
+        #region Fields
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+        #endregion
+
+        #region Properties
+
+        public abstract float CostoLlamada
+        {
+            get;
+            
+        }
+
+        public float Duracion
+        {
+            get
+            {
+                return duracion;
+            }
+        }
+
+        public string NroDestino
+        {
+            get
+            {
+                return nroDestino;
+            }
+        }
+
+        public string NroOrigen
+        {
+            get
+            {
+                return nroOrigen;
+            }
+        }
+        #endregion
+
+        #region Methods
+        public Llamada(float duracion,string nroDestino,string nroOrigen)
+        {
+            this.duracion = duracion;
+            this.nroDestino = nroDestino;
+            this.nroOrigen = nroOrigen;
+           
+        }
+
+        public virtual string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Duracion: {0}", duracion);
+            sb.AppendFormat("NUmero destino: {0} ",nroDestino);
+            sb.AppendFormat("Numero origen: {0}",nroOrigen);
+
+            return sb.ToString();
+
+        }
+
+        public static int OrdenarPorDuracion(Llamada llamada1,Llamada llamada2)
+        {
+            int retorno = 0;
+            if(llamada1.duracion<llamada2.duracion)
+            {
+                retorno = -1;
+            }
+            else if(llamada1.duracion > llamada2.duracion)
+                {
+                retorno = 1;
+                }
+
+
+
+
+            return retorno;
+        }
+
+        public static bool operator ==( Llamada l1,Llamada l2)
+        {
+            bool retorno = false;
+            if (l1.Equals(l2) && l1.nroDestino == l2.nroDestino && l1.nroOrigen == l2.nroOrigen)
+            {
+                retorno = true;
+            }
+            return retorno;
+
+        }
+        public static bool operator !=(Llamada l1,Llamada l2)
+        {
+            return !(l1 == l2);
+        }
+        #endregion
+
+        #region Nested TYpes
+        public enum TipoLlamada
+        {
+            Local,
+            Provincial,
+            Todas
+        }
+        #endregion
+
+    }
+}
---
+++ Ejercicio_40 -/BiblioteCentralTelefonica/Local.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioteCentralTelefonica
+{
+    public class Local : Llamada
+    {
+        #region Fields
+        protected float costo;
+
+        #endregion
+
+        #region Properties
+        public override float CostoLlamada
+        {
+            get
+            {
+                return CalcularCosto();
+            }
+        }
+        #endregion 
+
+        #region Methods
+
+        private float CalcularCosto()
+        {
+            float valor = this.duracion * this.costo;
+
+            return valor;
+        }
+
+
+        #endregion
+
+        public Local(Llamada llamada,float costo):this(llamada.NroOrigen,llamada.Duracion,llamada.NroDestino,costo)
+        {
+
+        }
+        public Local(string origen,float duracion,string destino,float costo):base(duracion,destino,origen)
+        {
+            this.costo=costo;
+
+        }
+        protected new string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("**Local**");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("Costo llamada: " + CostoLlamada.ToString());
+
+
+            return sb.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool retorno = false;
+            if (!(obj is null))
+            {
+                if (obj is Local)
+                {
+                    retorno = true;
+                }
+            }
+            return retorno;
+        }
+
+        public override string ToString()
+        {
+            return base.Mostrar();
+        }
+
+    }
+}
