a80d29a35f3acb04018b40d6c05a0bbe637c6998
zanonicintia <cintu94@hotmail.com>
2019-10-14 18:55:54 -0300

    sin terminar

--- 20191010-Zanoni.Cintia.2c/FrmCantina/Form1.cs
+++ 20191010-Zanoni.Cintia.2c/FrmCantina/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Entidades;
+using ControlCantina;
+
+
 
 namespace FrmCantina
 {
     public partial class Form1 : Form
     {
+        Barra barra=new Barra();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
