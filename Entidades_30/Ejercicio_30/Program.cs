8aaa7f480588effb7093aa5ab359a2a2d041ed62
zanonicintia <cintu94@hotmail.com>
2019-10-23 18:54:47 -0300

    actual

---
+++ Entidades_30/Ejercicio_30/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades_30;
+
+namespace Ejercicio_30
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Competencia c = new Competencia();
+        }
+    }
+}
