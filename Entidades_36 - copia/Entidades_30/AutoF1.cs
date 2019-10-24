e0aca4182437e7665046aed5e53c60e47777b60d
zanonicintia <cintu94@hotmail.com>
2019-10-24 14:44:06 -0300

    no hereda :(

---
+++ Entidades_36 - copia/Entidades_30/AutoF1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Entidades_30
+{
+    public class AutoF1: VehiculoDeCarrera
+    {
+        
+        private short caballosDeFuerza;
+        public short CaballosDeFuerza
+        {
+            get { return caballosDeFuerza; }
+            set { caballosDeFuerza = value; }
+        }
+
+        public AutoF1(short numero, string escuderia):base(numero,escuderia)
+        {
+      
+        }
+
+        public AutoF1(short numero,string escuderia,short caballosDeFuerza):this(numero,escuderia)
+        {
+            this.caballosDeFuerza = caballosDeFuerza;
+        }
+
+        public override string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0}", base.MostrarDatos());
+            sb.AppendFormat("Caballos de fuerza: {0} \n", this.caballosDeFuerza);
+            return sb.ToString();
+        }
+
+
+
+
+        public static bool operator ==(AutoF1 a1, AutoF1 a2)
+        {
+            bool retorno = false;
+            if (a1.escuderia == a2.escuderia && a1.numero == a2.numero 
+                && a1.caballosDeFuerza == a2.caballosDeFuerza)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(AutoF1 a1, AutoF1 a2)
+        {
+            return !(a1 == a2);
+        }
+
+
+    }
+
+   
+}
