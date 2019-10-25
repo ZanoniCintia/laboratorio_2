e4807df1443ffaff92f2be938cb0589add7db17e
alumno <alumno@utnfra.com.ar>
2019-10-24 21:04:21 -0300

    archivos

---
+++ Ejercicio_Clase_Archivos/ArchivoLocoForm/FormLoco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ArchivoLocoForm
+{
+    public partial class FormLoco : Form
+    {
+        private const string EXTENSION_VALIDA = ".txt";
+
+        public FormLoco()
+        {
+            InitializeComponent();
+        }
+    }
+}
