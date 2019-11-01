77035dc07c413f941fa142c1bbd18615c232afa4
alumno <alumno@utnfra.com.ar>
2019-10-31 22:16:16 -0300

    clase sql

---
+++ Ejercicio_57/Ejercicio_57/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_57
+{
+    public class Ejercicio_57
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Ejercicio_57/Entidades/Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Entidades
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+
+        public Persona()
+        {
+
+        }
+        public Persona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        public static  bool  Guardar(Persona p)
+        {
+            Persona persona = new Persona();
+
+            XmlTextWriter Writer;
+            XmlSerializer ser;
+            Writer = new XmlTextWriter(,Encoding.UTF8);
+            ser = new XmlSerializer(typeof(Persona));
+
+            ser.Serialize(Writer, p);
+
+            Writer.Close();
+
+            return true;
+
+        }
+    }
+}
