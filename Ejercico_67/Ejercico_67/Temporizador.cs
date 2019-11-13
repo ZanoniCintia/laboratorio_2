f601e97a2099eeb1a5e389ddc50a5c1d3320ee67
alumno <alumno@utnfra.com.ar>
2019-11-13 20:18:16 -0300

    ejercicio 67

---
+++ Ejercico_67/Ejercico_67/Temporizador.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Entidades_67
+{
+    public sealed  class Temporizador
+    {
+        private Thread hilo;
+        private int intervalo;
+
+        public bool Activo
+        {
+            get
+            {
+                bool retorno = false;
+                if(!(hilo is null) && this.hilo.IsAlive)
+                {
+                    retorno = true;
+                    
+                }
+
+                return retorno;
+            }
+            set
+            {   
+                if((Activo == false) && (value== true))
+                {
+                    Thread hilo = new Thread(Corriendo);
+                    hilo.Start();
+                }
+                else if((Activo == true) && (value == false))
+                {
+                    hilo.Abort();
+                }
+                
+            }
+        }
+
+        private void Corriendo()
+        {
+            while (true)
+            {
+                Thread.Sleep(250);
+                if(this.EventoTiempo != null)
+                {
+                    EventoTiempo.Invoke();
+                }
+            }
+        }
+
+        public event EncargadoTiempo EventoTiempo;
+        
+
+
+        public int Intervalo
+        {
+            get { return this.intervalo; }
+            set { intervalo = value; }
+        }
+
+
+
+        public delegate void EncargadoTiempo();
+    }
+}
---
+++ Ejercico_67/Prueba_67/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades_67;
+
+namespace Prueba_67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
